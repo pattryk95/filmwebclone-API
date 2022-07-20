c3917328b9968c90af259f366d009cad42107866
Patryk <patryk.bogdanski95@gmail.com>
2022-07-20 19:51:57 +0200

    Add small refactoring

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace filmwebclone_API.Controllers
+{
+    [Route("api/genres")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        public GenresController()
+        {
+
+        }
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Entities/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace filmwebclone_API.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
