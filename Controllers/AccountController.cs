5dc207d341a4b674c48697dab219a38e0b9b1e20
Patryk <patryk.bogdanski95@gmail.com>
2022-08-22 20:05:33 +0200

    Add registration and login functionality

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using filmwebclone_API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace filmwebclone_API.Controllers
+{
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<AuthenticationResponse>> Create(
+            [FromBody] UserCredentials userCredentials)
+        {
+            var user = new IdentityUser { UserName = userCredentials.Email, Email = userCredentials.Email };
+            var result = await _userManager.CreateAsync(user);
+
+            if (result.Succeeded)
+            {
+                return BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthenticationResponse>> Login([FromBody] UserCredentials userCredentials)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userCredentials.Email, userCredentials.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest("Incorrent Login");
+            }
+        }
+        private AuthenticationResponse BuildToken(UserCredentials userCredentials)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("email", userCredentials.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["keyjwt"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            var token = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiration, signingCredentials: creds);
+
+            return new AuthenticationResponse()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+    }
+}
---
+++ Models/AuthenticationResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filmwebclone_API.Models
+{
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
